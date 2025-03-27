bfee6904aef3c8ee5316d010b2ed1d8dc7000755
bhoomitech <bhoomitechzone@gmail.com>
2025-03-27 11:54:39 +0530

    chvdclccffggdhrvdvdvsvgdccccxcxcccllcvxsvxxcfcsccc

--- resources/views/Admin/pages/warning.blade.php
+++ resources/views/Admin/pages/warning.blade.php
@@ -674,33 +674,33 @@
 
             <!-- delete warning modal  -->
              <!-- Delete Start Here -->
-<div id="deletemodal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title" style="color: black;" id="exampleModalLabel">Delete Lead</h4>
-            </div>
-            <!-- Modal Body -->
-            <div class="modal-body">
-                <p>Are you sure you want to delete this? This action cannot be undone.</p>
-                <form id="delete_form" action="javascript:void(0);" method="post">
-                    @csrf
-                    <input type="hidden" name="id" id="delete_id" value="" class="form-control">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="d-flex justify-content-between" style="margin-top: 15px;">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                <button type="submit" class="btn btn-primary btn_delete">Delete</button>
-                            </div>
+            <div id="deletemodal" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title" style="color: black;" id="exampleModalLabel">Delete Warning</h4>
+                        </div>
+                        <!-- Modal Body -->
+                        <div class="modal-body">
+                            <p>Are you sure you want to delete this? This action cannot be undone.</p>
+                            <form id="delete_form" action="javascript:void(0);" method="post">
+                                @csrf
+                                <input type="hidden" name="id" id="delete_id" value="" class="form-control">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="d-flex justify-content-between" style="margin-top: 15px;">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                            <button type="submit" class="btn btn-primary btn_delete">Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
 <!-- Delete End Here -->
         </div>
     </div>
@@ -1061,7 +1061,7 @@
                     var formData = new FormData(this);
                     $.ajax({
                         type: "POST",
-                        url: "{{url('/delete_warning')}}",
+                        url: "{{url('/delete_warning_id')}}",
                         data: formData,
                         dataType: "json",
                         contentType: false,
